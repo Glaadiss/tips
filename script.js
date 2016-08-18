77692ff310772c84e64b008436591fdcedece85f
Glaadiss <bartekgladys@gmail.com>
2016-08-18 12:45:23 +0200

    tips works

---
+++ script.js
@@ -0,0 +1,135 @@
+$(document).ready(function(){
+
+   var i = 1;
+   var gender='';
+   var email='';
+   var name ='';
+   var age=[];
+   var email='';
+
+
+   window.fbAsyncInit = function() {
+     FB.init({
+       appId      : '855169464614612',
+       xfbml      : true,
+       version    : 'v2.7'
+
+     });
+     FB.Canvas.setAutoGrow();
+
+     function onLogin(response) {
+       if (response.status == 'connected') {
+         FB.api('/me?fields=id,name,first_name,birthday,gender,age_range,email', function(data) {
+           console.log(data);
+           gender = data.gender;
+           age= data.age_range;
+           name= data.first_name;
+           email = data.email
+           $('#facebook_name').html(''+name+'');
+           $('#nam').val(''+name+'');
+           $('#emai').val(''+email+'')
+         });
+       }
+     }
+
+     FB.getLoginStatus(function(response) {
+       // Check login status on load, and if the user is
+       // already logged in, go directly to the welcome message.
+       if (response.status == 'connected') {
+         onLogin(response);
+       } else {
+         // Otherwise, show Login dialog first.
+         FB.login(function(response) {
+           onLogin(response);
+         }, {scope: 'user_friends, email'});
+       }
+     });
+   };
+
+   (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+  $('#page_'+i+'').slideDown();
+
+  $('.next').on('click',function(){
+    $('#page_1').fadeOut().after(function(){
+      $('#page_2').slideDown('slow');
+    })
+  })
+
+  $('.dieta').on('click',function(){
+    $('.page').fadeOut().after(function(){
+      $('#hint').fadeOut();
+      $('.tab').fadeOut();
+      setTimeout(function(){
+        $('#page_2').slideDown('').after(function(){
+          $('#dieta').slideDown('slow');
+        })
+      },500)
+
+    })
+  })
+
+  $('.trening').on('click',function(){
+    $('.page').fadeOut().after(function(){
+      $('#hint').fadeOut();
+      $('.tab').fadeOut();
+      setTimeout(function(){
+        $('#page_2').slideDown('').after(function(){
+          $('#trening').slideDown('slow');
+        })
+      },500)
+
+    })
+  })
+  $('.supl').on('click',function(){
+    $('.page').fadeOut().after(function(){
+      $('#hint').fadeOut();
+      $('.tab').fadeOut();
+      setTimeout(function(){
+        $('#page_2').slideDown('').after(function(){
+          $('#supl').slideDown('slow');
+        })
+      },500)
+    })
+  })
+
+  $('.pyt').on('click', function(){
+    var askIndex = $('.pyt').index(this);
+    if($($('.odp').get(askIndex)).css('display') == 'none' ){
+      $('.odp').fadeOut().after(function(){
+        setTimeout(function(){
+            $($('.odp').get(askIndex)).slideDown('slow');
+        },500)
+      });
+    }
+    else{
+      $('.odp').slideUp()
+    }
+  })
+
+  $('.form_btn').on('click', function(){
+    $('.page').fadeOut().after(function(){
+      setTimeout(function(){
+        $('#page_3').slideDown('slow');
+      },500);
+    })
+  })
+
+  $('#sendEmail').on('click', function(){
+    $.ajax({
+      url: "https://formspree.io/bartekgladys@gmail.com",
+      method: "POST",
+      data: {pytanie: $('#pytanie').val(), imie: $('#nam').val(), email: $('#emai').val() },
+      dataType: "json"
+    }).success(function(){
+      $('#pytanie').val('');
+    })
+  })
+
+})
